3739d042378111ea73dbb784d5680642f54f764a
Ethan N <thanhnx.605@gmail.com>
2014-09-16 21:59:14 +0700

    Add Settings VC

---
+++ fanto/fanto/viewcontrollers/settings/FTSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTSettingsViewController.h
+//  fanto
+//
+//  Created by Ethan Nguyen on 9/16/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FTSettingsViewController : BaseViewController
+
+@end
---
+++ fanto/fanto/viewcontrollers/settings/FTSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FTSettingsViewController.m
+//  fanto
+//
+//  Created by Ethan Nguyen on 9/16/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "FTSettingsViewController.h"
+
+@interface FTSettingsViewController ()
+
+@end
+
+@implementation FTSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
