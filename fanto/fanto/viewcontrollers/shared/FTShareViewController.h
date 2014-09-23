01a60efbaae40a438be7f86c48529f7b3e5e4c33
Ethan N <thanhnx.605@gmail.com>
2014-09-23 20:07:36 +0700

    Add Share vc

---
+++ fanto/fanto/viewcontrollers/shared/FTShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTShareViewController.h
+//  fanto
+//
+//  Created by Ethan Nguyen on 9/23/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FTShareViewController : BaseViewController
+
+@end
---
+++ fanto/fanto/viewcontrollers/shared/FTShareViewController.m
@@ -0,0 +1,38 @@
+//
+//  FTShareViewController.m
+//  fanto
+//
+//  Created by Ethan Nguyen on 9/23/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "FTShareViewController.h"
+
+@interface FTShareViewController ()
+
+@end
+
+@implementation FTShareViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
