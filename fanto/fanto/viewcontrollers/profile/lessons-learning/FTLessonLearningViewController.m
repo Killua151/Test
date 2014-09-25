08318673bb04c6d0cda7b602e3c97d7f4e0f7284
Ethan N <thanhnx.605@gmail.com>
2014-09-25 18:53:05 +0700

    Refine

--- fanto/fanto/viewcontrollers/profile/lessons-learning/FTLessonLearningViewController.m
+++ fanto/fanto/viewcontrollers/profile/lessons-learning/FTLessonLearningViewController.m
@@ -11,9 +11,12 @@
 @interface FTLessonLearningViewController () {
   NSInteger _totalLessonsCount;
   NSInteger _currentLessonIndex;
+  NSInteger _totalHeartsCount;
+  NSInteger _currentHeartsCount;
 }
 
 - (void)setupViews;
+- (void)updateHeaderViews;
 
 @end
 
@@ -23,7 +26,8 @@
   [super viewDidLoad];
   
   _totalLessonsCount = 20;
-  _currentLessonIndex = 10;
+  _currentLessonIndex = 0;
+  _totalHeartsCount = _currentHeartsCount = 4;
   
   [self setupViews];
   [self reloadContents];
@@ -34,18 +38,16 @@
 }
 
 - (void)reloadContents {
-  [_btnProgressSegments enumerateObjectsUsingBlock:^(UIButton *button, NSUInteger index, BOOL *stop) {
-    button.selected = index <= _currentLessonIndex;
-    
-    if (index == _currentLessonIndex) {
-      CGPoint center = button.center;
-      center.y -= 7;
-      _imgAntProgressIndicator.center = center;
-    }
-  }];
+  [self updateHeaderViews];
 }
 
 - (IBAction)btnClosePressed:(UIButton *)sender {
+  _currentLessonIndex++;
+  
+  if (_currentLessonIndex % 5 == 0)
+    _currentHeartsCount--;
+  
+  [self updateHeaderViews];
 }
 
 - (IBAction)btnHeartPotionPressed:(UIButton *)sender {
@@ -55,8 +57,19 @@
 - (void)setupViews {
   _lblLessonsCount.font = [UIFont fontWithName:@"ClearSans" size:17];
   
-  for (UIButton *button in _btnHearts)
+  [_btnHearts enumerateObjectsUsingBlock:^(UIButton *button, NSUInteger index, BOOL *stop) {
     button.selected = YES;
+    button.hidden = index >= _totalHeartsCount;
+  }];
+  
+  CGRect frame = _vHearts.frame;
+  frame.size.width = _totalHeartsCount * 30;
+  frame.origin.x = self.view.frame.size.width - 15 - frame.size.width;
+  _vHearts.frame = frame;
+  
+  frame = _btnHeartPotion.frame;
+  frame.origin.x = _vHearts.frame.origin.x - frame.size.width - 13;
+  _btnHeartPotion.frame = frame;
   
   [_btnProgressSegments enumerateObjectsUsingBlock:^(UIButton *button, NSUInteger index, BOOL *stop) {
     [button setBackgroundImage:[UIImage imageFromColor:UIColorFromRGB(158, 158, 158)] forState:UIControlStateNormal];
@@ -64,6 +77,26 @@
     button.selected = NO;
     
     button.hidden = index >= _totalLessonsCount;
+  }];
+}
+
+- (void)updateHeaderViews {
+  _lblLessonsCount.text = [NSString stringWithFormat:@"%ld/%ld", (long)_currentLessonIndex+1, (long)_totalLessonsCount];
+  
+  [_btnHearts enumerateObjectsUsingBlock:^(UIButton *button, NSUInteger index, BOOL *stop) {
+    button.selected = index >= _totalHeartsCount - _currentHeartsCount;
+  }];
+  
+  _imgAntProgressIndicator.hidden = _currentLessonIndex < 0;
+  
+  [_btnProgressSegments enumerateObjectsUsingBlock:^(UIButton *button, NSUInteger index, BOOL *stop) {
+    button.selected = index <= _currentLessonIndex;
+    
+    if (index == _currentLessonIndex) {
+      CGPoint center = button.center;
+      center.y -= 7;
+      _imgAntProgressIndicator.center = center;
+    }
   }];
 }
 
