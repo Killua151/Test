52650d361c51ce778fecd03ffd3ad4af216f9179
Ethan N <thanhnx.605@gmail.com>
2014-09-25 13:59:11 +0700

    Add Lessons learning folder

---
+++ fanto/fanto/viewcontrollers/profile/lessons-learning/FTLessonLearningViewController.h
@@ -0,0 +1,13 @@
+//
+//  FTLessonLearningViewController.h
+//  fanto
+//
+//  Created by Ethan on 9/25/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FTLessonLearningViewController : BaseViewController
+
+@end
---
+++ fanto/fanto/viewcontrollers/profile/lessons-learning/FTLessonLearningViewController.m
@@ -0,0 +1,25 @@
+//
+//  FTLessonLearningViewController.m
+//  fanto
+//
+//  Created by Ethan on 9/25/14.
+//  Copyright (c) 2014 Ethan Nguyen. All rights reserved.
+//
+
+#import "FTLessonLearningViewController.h"
+
+@interface FTLessonLearningViewController ()
+
+@end
+
+@implementation FTLessonLearningViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+}
+
+@end
