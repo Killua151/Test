ab102d490ce699f1b8d09bbb199b3009c928933d
Ethan N <thanhnx.605@gmail.com>
2014-10-22 09:52:03 +0700

    Refine

--- memo/memo/viewcontrollers/home/MMHomeViewController.m
+++ memo/memo/viewcontrollers/home/MMHomeViewController.m
@@ -121,7 +121,7 @@
     imgSlide.frame = frame;
     [vSlideImage addSubview:imgSlide];
     
-    [_vSlide addSubview:vSlideImage];
+    [_vSlide addSubview:imgSlide];
   }
   
   _btnLogIn.titleLabel.font = [UIFont fontWithName:@"ClearSans-Bold" size:17];
